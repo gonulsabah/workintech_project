2d34b541c1c3ff9b07d75f93a22259abef0c3c0a
Gönül SABAH <gonulsabah@gmail.com>
2026-04-27 20:48:33 +0300

    data-sellers/ added

--- olist/utils.py
+++ olist/utils.py
@@ -14,3 +14,19 @@ def haversine_distance(lon1, lat1, lon2, lat2):
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     return 2 * 6371 * asin(sqrt(a))
 
+def return_significative_coef(model):
+    """
+    Returns p_value, lower and upper bound coefficients
+    from a statsmodels object.
+    """
+    # Extract p_values
+    p_values = model.pvalues.reset_index()
+    p_values.columns = ['variable', 'p_value']
+
+    # Extract coef_int
+    coef = model.params.reset_index()
+    coef.columns = ['variable', 'coef']
+    return p_values.merge(coef,
+                          on='variable')\
+                   .query("p_value<0.05").sort_values(by='coef',
+                                                      ascending=False)
