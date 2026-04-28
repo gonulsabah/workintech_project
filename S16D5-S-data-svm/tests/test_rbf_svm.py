558652fb10d57567df4558a53694a7de23fb585d
Gönül SABAH <gonulsabah@gmail.com>
2026-04-28 21:01:44 +0300

    S16D5-S-data-svm completed

---
+++ S16D5-S-data-svm/tests/test_rbf_svm.py
@@ -0,0 +1,12 @@
+from nbresult import ChallengeResultTestCase
+
+
+class TestRbfSvm(ChallengeResultTestCase):
+
+    def test_hyperparams(self):
+
+        self.assertGreaterEqual(self.result.best_gamma, 0.1)
+        self.assertLess(self.result.best_gamma, 5)
+
+        self.assertGreaterEqual(self.result.best_c, 1)
+        self.assertLess(self.result.best_c, 1000)
