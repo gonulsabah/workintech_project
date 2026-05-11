475ff9c5a30a5efdf328f8fb6dcc0b9764dd1620
Gönül SABAH <gonulsabah@gmail.com>
2026-05-11 19:37:30 +0300

    S17D2-S-kaggle-competition added

---
+++ S17D2-S-kaggle-competition/tests/test_features_overview.py
@@ -0,0 +1,6 @@
+from nbresult import ChallengeResultTestCase
+
+class TestFeaturesOverview(ChallengeResultTestCase):
+
+    def test_feat_categorical_small(self):
+        self.assertEqual(self.result.n, 34)
